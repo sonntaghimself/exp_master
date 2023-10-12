5775f79a937f58e6cd433a237b1d4ef5dcfb44c9
Samuel Sonntag <samuel@sonntag.tv>
2023-10-12 21:24:43 +0200

    as far as i know, done

---
+++ experiment.py
@@ -0,0 +1,235 @@
+"""
+This is the file where the actual experiment will be programmed.
+
+To start with, helpers will be imported and the participant information stored
+in a seperate file (order will be randomized to assure anonymity)
+
+the trial list will be randomized at the beginning of each block. Will it
+though?
+"""
+
+from psychopy import visual, event, core, gui
+import datetime as dt
+import pandas as pd
+import random
+import helpers
+
+########################
+#   global variables   #
+########################
+parameters = {
+    "time": {"fix": 30, "feedback": 50, "iti": 30, "task": 30, "pres": 200},
+    "keys": ["s", "l"],
+    "start_key": "space",
+    "dotsize": 50,
+    "ndots": 200,
+    "proportions": {"large": 0.85, "small": 0.70},
+    "color": {"col_1": [0, 1, 0], "col_2": [1, 0, 0]},
+    "dist_col": {"col_1": [1, 0, 0], "col_2": [0, 1, 0]},
+    "dir": {"right": 0, "left": 180},
+    "dist_dir": {"right": 180, "left": 0},
+    "colnames": {"col_1": "green", "col_2": "red"},
+    "cor_resp_dir": {"right": "l", "left": "s"}
+}
+
+########################
+#    vp information    #
+########################
+vp_info = helpers.gather_information()
+
+if vp_info["version"] == "full":
+    parameters["num"] = {"nblks": 10, "pracblks": 2, "nprac": 16, "ntrls" : 40}
+elif vp_info["version"] == "test":
+    parameters["num"] = {"nblks": 2, "pracblks": 0, "nprac": 8, "ntrls" : 8}
+# NOTE: since we have 4 stimuli (and two tasks), the #of trials has to be divisible by 8 (& >= 8)
+
+########################
+#       Stimuli        #
+########################
+dir = ["right", "left"]
+col = ["col_1", "col_2"]
+tsk = ["color", "direction"]
+stimuli = [
+    [direction, color, task] for direction in dir for color in col for task in tsk
+]
+
+if vp_info["vp_num"] % 2:
+    parameters["cor_resp_col"] = {"col_1": "s", "col_2": "l"}
+else:
+    parameters["cor_resp_col"] = {"col_1": "l", "col_2": "s"}
+
+for stim in stimuli:
+    if stim[2] == "color":
+        stim.append(parameters["cor_resp_col"][stim[1]])
+    elif stim[2] == "direction":
+        stim.append(parameters["cor_resp_dir"][stim[0]])
+    if parameters["cor_resp_dir"][stim[0]] == parameters["cor_resp_col"][stim[1]]:
+        stim.append("congruent")
+    else:
+        stim.append("incongruent")
+
+########################
+#     import files     #
+########################
+files = helpers.my_files(vp_info)
+
+# Storing demographics
+helpers.demographics(vp_info, files)
+
+########################
+#      Trial Seq       #
+########################
+exp = helpers.randomisation(stimuli, vp_info, parameters, files)
+
+########################
+#     Instructions     #
+########################
+inst_text = helpers.read_instructions(files, parameters)
+
+########################
+#       Psychopy       #
+########################
+win = visual.Window(size=(1280, 800), color=(0, 0, 0), units="pix")
+timer = core.Clock()
+inst_stim = visual.TextStim(win, text=inst_text["inst"], alignText="center",
+                            wrapWidth=(0.75*win.size[0]))
+fb_stim = visual.TextStim(win)
+task_stim = visual.TextStim(win)
+fix_stim = visual.ShapeStim(
+    win,
+    lineWidth=2,
+    lineColor="white",
+    pos=(0, 0),
+    vertices=((-10, 0), (10, 0), (0, 0), (0, 10), (0, -10)),
+    closeShape=False,
+)
+
+dots_stim = visual.DotStim(
+    win, dotSize=20, coherence=1, dotLife=-1, speed=1, units="pix", fieldSize=win.size,
+    nDots = int(parameters["ndots"]*parameters["proportions"]["small"])
+)
+dots_dist = visual.DotStim(
+    win, dotSize=20, coherence=1, dotLife=-1, speed=1, units="pix",
+    fieldSize=win.size, nDots = int(parameters["ndots"]-dots_stim.nDots)
+)
+
+########################
+#   result variable    #
+########################
+# NOTE: this will also be a list of dictionaries with a dictionary for each
+# trial.
+
+for blk in exp:
+    blk = [x for x in blk if x] # making sure no faulty values are passed through
+
+    if blk[0]["blk"] == 1:
+        inst_stim.draw()
+        win.flip()
+        event.waitKeys(keyList=parameters["start_key"])
+
+    for trl in blk:
+        task_stim.text = trl["task"]
+        for _ in range(parameters["time"]["task"]):
+            task_stim.draw()
+            win.flip()
+        for _ in range(parameters["time"]["fix"]):
+            fix_stim.draw()
+            win.flip()
+
+        dots_stim.color = parameters["color"][trl["color"]]
+        dots_stim.dir = parameters["dir"][trl["direction"]]
+        dots_dist.color = parameters["dist_col"][trl["color"]]
+        dots_dist.dir = parameters["dist_dir"][trl["direction"]]
+
+        win.callOnFlip(timer.reset)
+
+        event.clearEvents()
+        trl_complete = False
+
+        frames = 0
+        slow = False
+
+        while not trl_complete:
+            dots_stim.draw()
+            dots_dist.draw()
+            win.flip()
+            frames += 1
+            keys = []
+            keys = event.getKeys()
+            if keys:
+                rt = core.getTime()
+                if trl["cor_resp"] in keys:
+                   corr = 1
+                   fb_stim.text = "Correct"
+                else:
+                   corr = 0
+                   fb_stim.text = "Incorrect"
+                trl_complete = True
+
+            if frames >= parameters["time"]["pres"]:
+                rt = core.getTime()
+                fb_stim.text = "Too slow"
+                trl_complete = True
+                corr = 0
+                slow = True
+
+        if "escape" in keys:
+            break
+
+        for _ in range(parameters["time"]["feedback"]):
+            fb_stim.draw()
+            win.flip()
+
+        trl["date"] = dt.datetime.now().strftime("%d-%m-%Y-%H:%M:%S")
+        trl["rt"] = rt
+        trl["corr"] = corr
+
+        for _ in range(parameters["time"]["iti"]):
+            win.flip()
+
+    blk_num = blk[0]["blk"]
+    num_trls = len(blk)
+    corr = [x["corr"] for x in blk]
+    blk_per = (corr.count(1) / num_trls) * 100
+
+    fb_txt = "Block {}, \n Correct: {}%".format(blk_num, blk_per)
+    fb_txt = fb_txt + "\n\nPress the spacebar to continue."
+    fb_stim.text = fb_txt
+    fb_stim.draw()
+    win.flip()
+    event.waitKeys(keyList=[parameters["start_key"]])
+
+    # blank screen for inter-trial-interval
+    for _ in range(parameters["time"]["iti"]):
+        win.flip()
+
+########################
+#         Data         #
+########################
+tmp_data = [trial for data in exp for trial in data if trial]
+dataDF = pd.DataFrame()
+dataDF = dataDF.from_dict(tmp_data)
+dataDF.to_csv(files["resfile"], header=True, index=False, sep=",", mode="w")
+
+
+if vp_info["gender"] == "male":
+    address = " dude !!"
+else:
+    address = " you are awesome !!!"
+
+end_text = "The Experiment is done, thank you so much for participating{}".format(
+    address
+)
+
+end_text = end_text + "\n\n may the force be with you."
+
+end_text = end_text + "\n\n press any key to end the experiment."
+
+fb_stim.text = end_text
+fb_stim.draw()
+win.flip()
+event.waitKeys()
+
+# close window and quit
+win.close()
+core.quit()
---
+++ helpers.py
@@ -0,0 +1,201 @@
+from psychopy import gui
+import datetime as dt
+import os
+import random
+import pandas as pd
+
+"""
+in general, these are all the little worker functions to get my experiment
+running.
+
+The function check_data is going to be responsible for checking my data and
+return a boolean that reflects whether the data format is correct or not.
+
+The function gather_information is going to be the one responsible for
+collecting the data; it will run recursively until check_data returns TRUE
+
+the function my_files will open and load some current information and enable us
+to save information in the current directory; it will take the vp_num as 
+arguments and create a results file).
+
+the function randomisation will take our trial list and randomise the
+presentation order; then return the randomised trial list.
+
+the function demographics will load the current dataframe with demographic
+information, add the current information and save the file again in a randomised
+order to guarantee anonymisation.
+"""
+
+gender = ["female", "male", "non-binary", "prefer not to answer"]
+
+########################
+#      check_data      #
+########################
+def check_data(ok_data):
+    """function to check whether the collected information is in the right
+    format and plausible, returns a boolean"""
+    check_passed = (
+        isinstance(ok_data[0], int)
+        and isinstance(ok_data[1], int)
+        and (17 < ok_data[1] < 100)
+        # and (ok_data[1] in gender) # don't need to be checked -> choices
+    )
+    return check_passed
+
+
+########################
+#  gather_information  #
+########################
+def gather_information():
+    """this function gathers (demographic) information about the participants 
+    (including whether to run the test-version or not) and returns a dictionary 
+    containing the information.
+    """
+    myDlg = gui.Dlg(title="Participant Information")
+    myDlg.addText('Subject info')
+    myDlg.addField('VP Number:', initial=1)
+    myDlg.addField('Age:', initial=18, tip="you have to be at least 18")
+    myDlg.addField('Gender:', choices=gender)
+    myDlg.addField('Handedness:', choices=["left", "right", "ambidextrous"])
+    myDlg.addField('version', choices=["full", "test"])
+    ok_data = myDlg.show()  # show dialog and wait for OK or Cancel
+    check_passed = check_data(ok_data)
+    if not check_passed:  # or if ok_data is not None
+        return gather_information()
+    else:
+        return {"vp_num": ok_data[0],
+                "age": ok_data[1], 
+                "gender": ok_data[2],
+                "handedness": ok_data[3],
+                "version": ok_data[4]
+                }
+
+########################
+#      open files      #
+########################
+def my_files(vp_info):
+    """this is the function that opens/creates necessary results files and
+    stores the paths to those files in the variable files."""
+    files = {"dirname": os.getcwd(), "vp_num": vp_info["vp_num"]}
+    files["expname"] = os.path.basename(files["dirname"])
+    files["date"] = dt.datetime.today().strftime("%d/%m/%Y")
+    files["insdir"] = files["dirname"] + os.sep + "Instructions"
+    files["resdir"] = files["dirname"] + os.sep + "Results"
+
+    if not os.path.isdir(files["resdir"]):
+        os.makedirs(files["resdir"])
+
+    files["resfile"] = files["expname"] + "_" + str(vp_info["vp_num"]) + ".res"
+
+    if os.path.isfile(files["resfile"]):
+        askUser = None
+        while askUser not in ["y", "n"]:
+            askUser = input("File exists! Overwrite? (y, n): ").lower()
+        if askUser == "y":
+            os.remove(files["resfile"])
+        elif askUser == "n":
+            sys.exit()
+    return files
+
+########################
+#    randomisation     #
+########################
+def randomisation(stimuli, vp_info, parameters, files):
+
+    all_blocks = parameters["num"]["pracblks"] + parameters["num"]["nblks"]
+
+    if parameters["num"]["ntrls"] < len(stimuli):
+        exp = [[{} for _ in range(len(stimuli))] for _ in range(all_blocks)]
+    elif parameters["num"]["ntrls"] >= len(stimuli):
+        exp = [
+            [{} for _ in range(parameters["num"]["ntrls"])] for _ in
+            range(all_blocks)
+        ]
+
+    for iblk, blk in enumerate(exp):
+
+        if iblk < parameters["num"]["pracblks"]:
+            nstim = parameters["num"]["nprac"]
+            practice = True
+        else:
+            nstim = parameters["num"]["ntrls"]
+            practice = False
+
+        if nstim <= len(stimuli):
+            stim_blk = stimuli
+        else:
+            stim_blk = stimuli * int(nstim / len(stimuli))
+
+        random.shuffle(stim_blk)
+
+        for itrl, trl in enumerate(blk):
+
+            if itrl >= len(stim_blk):
+                break
+
+            for i in vp_info:
+                trl[i] = vp_info[i]
+
+            trl["expname"] = files["expname"]
+            trl["blk"] = iblk + 1
+            trl["trl"] = itrl + 1
+            trl["practice"] = practice
+            trl["direction"] = stim_blk[itrl][0]
+            trl["color"] = stim_blk[itrl][1]
+            trl["task"] = stim_blk[itrl][2]
+            trl["cor_resp"] = stim_blk[itrl][3]
+            trl["congruency"] = stim_blk[itrl][4]
+
+    return exp
+
+########################
+#     demographics     #
+########################
+def demographics(vp_info, files):
+    """this function takes care of storing the demographic data seperately from
+    the other results and anonymises."""
+    my_file_path = files["dirname"] + os.sep + "demographics.csv"
+    df = pd.DataFrame()
+
+    if os.path.isfile(my_file_path):
+        # TODO: this is broken, somehow reading in the data doesn't want to work
+        vp_info_cur = pd.read_csv(filepath_or_buffer = my_file_path, index_col=None)
+
+        # NOTE: records solution
+        # vp_info_cur = vp_info_cur.to_dict(orient="records")
+        # vp_info_cur = [vp_info_cur]
+        # vp_info_cur.append(vp_info)
+
+        # NOTE: this is the old; list solution
+        vp_info_cur = vp_info_cur.to_dict(orient="list")
+        vp_info_cur["age"].append(vp_info["age"])
+        vp_info_cur["gender"].append(vp_info["gender"])
+        vp_info_cur["handedness"].append(vp_info["handedness"])
+        # anonymisation
+        # random.shuffle(vp_info_cur["age"])
+        # random.shuffle(vp_info_cur["gender"])
+        # random.shuffle(vp_info_cur["handedness"])
+    else: 
+        vp_info_cur = {"age": [vp_info["age"]], "gender": [vp_info["gender"]], "handedness": [vp_info["handedness"]]}
+
+    df = df.from_dict(vp_info_cur)
+    df.to_csv(my_file_path, header=True, index=False, sep=",", mode="w")
+
+########################
+#     Instructions     #
+########################
+def read_instructions(files, parameters):
+    txt_inst = {}
+    for inst in os.listdir(files["insdir"]):
+        if inst.endswith(".txt"):
+            with open(os.path.join(files["insdir"], inst), "r") as f:
+                txt_inst[os.path.splitext(inst)[0]] = f.read().format(
+                    parameters["cor_resp_col"]["col_1"].capitalize(),
+                    parameters["colnames"]["col_1"].capitalize(),
+                    parameters["cor_resp_col"]["col_2"].capitalize(),
+                    parameters["colnames"]["col_2"].capitalize(),
+                    parameters["cor_resp_dir"]["left"].capitalize(),
+                    parameters["cor_resp_dir"]["right"].capitalize(),
+                    parameters["start_key"]
+                )
+    return txt_inst
